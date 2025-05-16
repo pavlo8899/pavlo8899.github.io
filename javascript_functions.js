09f2ff951899ea5f53ed9fe8179f8461c9079312
pavlo8899 <pavlojano07@gmail.com>
2025-05-17 00:05:09 +0200

    Main page created

---
+++ javascript_functions.js
@@ -0,0 +1,4 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Ndalon dërgimin e formularit
+    alert('Faleminderit që na kontaktuat! Do të ju përgjigjemi sa më shpejt.');
+});
